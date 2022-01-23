f3a487593c278bfe3a5e218dd93ba6049f386ab0
susarlarayudu <rayuduroyal1996@gmail.com>
2022-01-23 12:49:51 +0530

    auto pushing chainges repo

---
+++ db/vars.tf
@@ -0,0 +1,5 @@
+variable "ENV" {}
+variable "MONGODB_INSTANCE_TYPE" {}
+variable "RABBITMQ_INSTANCE_TYPE" {}
+variable "REDIS_INSTANCE_TYPE" {}
+variable "MYSQL_INSTANCE_TYPE" {}
---
+++ vpc/vars.tf
@@ -0,0 +1,9 @@
+variable "VPC_CIDR_MAIN" {}
+variable "VPC_CIDR_ADDON" {}
+variable "PRIVATE_SUBNETS" {}
+variable "PUBLIC_SUBNETS" {}
+variable "AZS" {}
+variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
+variable "INTERNAL_HOSTEDZONE_ID" {}
+variable "PUBLIC_HOSTEDZONE_ID" {}
